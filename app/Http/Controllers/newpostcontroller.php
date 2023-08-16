8dc1bd9e575efc66922cb3e52f5676599dadd105
Ahany42 <ahanyfathy42@gmail.com>
2023-08-16 23:54:13 +0300

    user auth

--- app/Http/Controllers/newpostcontroller.php
+++ app/Http/Controllers/newpostcontroller.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 
 
 class newpostcontroller extends Controller {
     public function addnewpost(){
-    return view('newpost');
+        $user=Auth::user();
+    return view('newpost',['users'->$user]);
 }
 }
