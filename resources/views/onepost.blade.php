306a46182e061af1c86c0c58f1205ddbbaf64d9d
Khalifuu <khalifa15223011@gmail.com>
2023-08-17 00:37:57 +0200

    onepost

--- resources/views/onepost.blade.php
+++ resources/views/onepost.blade.php
@@ -11,49 +11,68 @@
             <div class="d-flex flex-start align-items-center">
               <div>
             <!-- Post Title -->
-                <h6 class="fw-bold text-primary mb-1">{{$post->post_title}}</h6>
+                <h3 class="fw-bold text-primary mb-1">{{$post->post_title}}</h3>
                
               </div>
             </div>
             <!-- content -->
-            <p style="font-weight: 1000 ;">
+            <p style="font-weight:1000;">
               {{$post->post_body}}
             </p>
             <hr>
-@endforeach
             <!-- Show Old Comments Related to this post -->
-            @foreach($comments as $comment)
-            <div>
-                  <p>
-                    {{$comment->comment_body}} <br>
-                  </p>  
+            <div class="d-flex">
 
+                <div>
+                @foreach($users as $user)
+                
+                <h4>
+                {{$user->name}}
+                
+                </h4>
+               
+                 @endforeach
+                </div>
+                    <div>
+                    @foreach($comments as $comment)
+                    
+                        <p>
+                           
+                            {{$comment->comment_body}} 
+                        </p>
+                        @endforeach
+                    </div>
+                    
+                    
+                    
             </div>
-            <hr>
-            @endforeach
-            
+                    
             <div class="small d-flex justify-content-start">
               <a href="#textAreaExample" class="d-flex align-items-center me-3">
                 <i class="far fa-comment-dots me-2"></i>
                 <!-- drop into text box -->
                 <p class="mb-0">Comment</p>
               </a>
-             
+              
             </div>
           </div>
           <div class="card-footer py-3 border-0" style="background-color: #f8f9fa;">
             <div class="d-flex flex-start w-100">
-              <div class="form-outline w-100">
-              @auth
-                <textarea class="form-control" id="textAreaExample" rows="4"
-                  style="background: #fff;"></textarea>
-              </div>
+                <div class="form-outline w-100">
+            <form action="/onepost/{{$post->id}}" method="post">
+                @csrf
+                    <textarea class="form-control" id="newcomment" name="comment_body" rows="4"
+                    style="background: #fff;"></textarea>
+                </div>
             </div>
             <div class="float-end mt-2 pt-1">
                 <!-- Add new Comment -->
-              <button type="button" class="btn btn-primary btn-sm">Post comment</button>
-              <!-- back to the Home -->
-              @endauth
+                <button type="submit" class="btn btn-primary btn-sm">Post comment</button>
+                <!-- back to the Home -->
+            </form>
+            
+            @endforeach
+            
               <a href="/Home">
                   <button type="button" class="btn btn-outline-primary btn-sm" >Home</button>
                 </a>
